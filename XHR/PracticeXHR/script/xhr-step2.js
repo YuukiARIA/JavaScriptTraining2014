5cce2f09826e91b693b00e820efd68893f68f930
YuukiARIA <yuuki.aria.del.viscontie@gmail.com>
2014-04-18 13:17:53 +0900

    Step 2

--- XHR/PracticeXHR/script/xhr-step2.js
+++ XHR/PracticeXHR/script/xhr-step2.js
@@ -2,32 +2,32 @@
 window.onload = function() {
 
     // 自分のhostnameに変更する
-    var path = "http://HOSTNAME:PORT/result/";
+    var path = "http://90202024n.local:8000/result/";
 
     // xhrの準備
-    @@@@
-    @@@@
+    var xhr = new XMLHttpRequest;
+    xhr.onreadystatechange = drawResult;
 
-    var select = document.getElementById(@@@@);
+    var select = document.getElementById('bloodTypeBox');
     select.onchange = function() {
         // 選択された値を取得する
-        var selectedOption = this.options[ this.selectedIndex ];
+	var selectedOption = this.options[ this.selectedIndex ];
         var selectedBloodType = selectedOption.value;
         //alert (selectedBloodType);
 
         if (selectedBloodType !== "default") {
             // urlを作成し、xhrでtxtファイルを取得する
-            var url = path + "type-" + @@@@ + ".txt";
-            @@@@
-            @@@@
+            var url = path + "type-" + selectedBloodType + ".txt";
+            xhr.open("get", url, true);
+            xhr.send();
         }
-    }
+    };
 
     function drawResult() {
         if((xhr.readyState === 4) && (xhr.status === 200)) {
             //alert(xhr.responseText);
-            var resultArea = document.getElementById(@@@@);
-            resultArea.textContent = @@@@;
+            var result = xhr.responseText.trim();
+            document.getElementById('resultArea').textContent = result;
         }
     }
 }
